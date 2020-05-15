13c79fa5b01eb442567fdbb2bf41ae8d79fceb42
chris <chris@192.168.0.28>
2020-05-15 13:34:18 -0700

    init commit - CJ

---
+++ MyStore_Boot/src/main/java/com/javaminions/MyStoreBootApplication.java
@@ -0,0 +1,13 @@
+package com.javaminions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreBootApplication.class, args);
+	}
+
+}
