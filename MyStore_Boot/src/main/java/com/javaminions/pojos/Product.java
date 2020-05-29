0e5db1d44ef13eae489ec5a483b71e0f644f900b
Vonx <indexroolz@yahoo.com>
2020-05-29 15:46:40 -0700

    lombok error fix

--- MyStore_Boot/src/main/java/com/javaminions/pojos/Product.java
+++ MyStore_Boot/src/main/java/com/javaminions/pojos/Product.java
@@ -9,14 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Product implements Serializable{
 
 	@Id
